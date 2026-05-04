8cf9672d6bd36b891466540ce0c771349c7e9678
Muhammad Giffari <alyarubi.998@gmail.com>
2026-05-04 11:39:17 +0700

    add function getUserInput

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
 console.log("Hello AI!!");
+
+// Function untuk mendapatkan input dari user
+function getUserInput(message) {
+  return prompt(message);
+}
+
+// Contoh penggunaan
+const userName = getUserInput("Masukkan nama Anda:");
+console.log("Halo, " + userName + "!");
